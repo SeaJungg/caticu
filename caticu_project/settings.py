3435bac2b988ebedec43337a4b291f320dabfd98
SeaJungg <wantedsaea@likelion.org>
2021-05-09 01:45:13 +0900

    오타수정3

--- caticu_project/settings.py
+++ caticu_project/settings.py
@@ -38,7 +38,10 @@ SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".ap-northeast-2.compute.amazonaws.com",
+    '127.0.0.1',
+]
 
 
 # Application definition
